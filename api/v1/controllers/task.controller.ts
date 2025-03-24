e12f28e6fbdaa8b83b9fd7b927ee00e876f8b524
HoangAnh <hoanganh52521352@gmail.com>
2025-03-24 08:30:19 +0700

    11. Bộ lọc theo trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,9 +2,20 @@ import {Response, Request} from "express"
 import { Task } from "../../../model/task.model";
 
 export const index = async (req: Request, res: Response) => {
-  const tasks = await Task.find({
+  interface Find {
+    deleted: boolean,
+    status?: string
+  }
+
+  const find: Find = {
     deleted: false
-  })  
+  }
+
+  if(req.query.status) {
+    find.status = req.query.status.toString();
+  }
+
+  const tasks = await Task.find(find)  
 
   console.log(tasks);
 
