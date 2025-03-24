328abe9312aa29f8afca891df8bef8528790327a
HoangAnh <hoanganh52521352@gmail.com>
2025-03-24 23:34:01 +0700

    Thêm auth cho các route tasks

--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -1,13 +1,18 @@
 import {Express} from "express";
 import { taskRoutes } from "./task.route";
 import { userRoutes } from "./user.route";
+import * as authMiddleware from "../middlewares/auth.middlewares";
 
 const mainV1Routes = (app: Express): void => {
   const version: string = "/api/v1";
 
   app.use(version + "/tasks", taskRoutes);
 
-  app.use(version + "/users", userRoutes);
+  app.use(
+    version + "/users", 
+    authMiddleware.requireAuth,
+    userRoutes
+  );
 }
 
 export default mainV1Routes;
