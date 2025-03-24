bd3742bff51a02b6c624e6dfd3d87f083559d018
HoangAnh <hoanganh52521352@gmail.com>
2025-03-24 15:13:58 +0700

    4. Đổi trạng thái nhiều công việc

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -8,6 +8,8 @@ router.get("/detail/:id", controller.detail)
 
 router.patch("/change-status/:id", controller.changeStatus)
 
+router.patch("/change-multi", controller.changeMulti)
+
 export const taskRoutes: Router = router;
 
 
